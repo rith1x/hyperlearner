90dd65c019fd5ab10332131452c568a0bcbbbdb0
rith1x <kiruthikkumarmpc24@gmail.com>
2023-10-20 12:14:30 +0530

    base version

---
+++ script.js
@@ -0,0 +1,16 @@
+
+window.addEventListener("load", function() {
+  var loader = document.getElementById("loader");
+  loader.classList.remove("is-active");
+});
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
